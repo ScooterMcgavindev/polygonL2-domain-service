a3ec5a6c08d8ed50b0017b09407706d07c3cc3d7
ScooterMcgavin21 <scott.skrobel@gmail.com>
2022-03-07 19:20:04 -0700

    register domains on chain

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+    // map domain string to wallet address to store domain names
+    mapping(string => address) public domains;
+
     constructor() {
         console.log("THIS IS MY DOMAINS CONTRACT. HELLA.");
+    }
+    //Tado: add function people can hit and register domain name with a place to store names
+    // register names to add to mapping, msg.sender's address is that who called the func
+    function register(string calldata name) public {
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain", msg.sender);
+    }
+    // returns domain ownders address
+    // calldata: indicates the location of where the name arg is stored
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,18 @@
 const main = async () => {
+  // grab wallet address of contract owner along with a random wallet
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
   console.log('Contract deployed to:', domainContract.address);
+  console.log('Contract deployed by:', owner.address);
+
+  // calling function to register name 'scoot'
+  const txn = await domainContract.register('scoots');
+  await txn.wait();
+  // call get address function to return the owner of domain
+  const domainOwner = await domainContract.getAddress('scoots');
+  console.log('Owner of domain:', domainOwner);
 };
 
 const runMain = async () => {
