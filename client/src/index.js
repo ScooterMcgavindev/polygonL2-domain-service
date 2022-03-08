b8c3416f28e13dd4ccc1a6852e6bccdfe7378372
ScooterMcgavin21 <scott.skrobel@gmail.com>
2022-03-07 22:05:20 -0700

    inital clientside page with webpack

---
+++ client/src/index.js
@@ -0,0 +1,4 @@
+import { render } from 'react-dom';
+import App from './components/App';
+import './styles/index.css';
+render(<App />, document.getElementById('root'));
---
+++ client/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+let mode = 'development';
+const plugins = [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin(),
+  new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })
+];
+
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+module.exports = {
+  mode: mode,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]'
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+    hot: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i, // sass, scss, & css
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '' }
+          },
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset'
+      }
+    ]
+  },
+
+  plugins: plugins
+};
